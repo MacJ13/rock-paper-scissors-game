a88a45d7eb5820e5d7af7124de9e233300553aac
MacJ13 <jacdim02@gmail.com>
2023-02-03 12:02:17 +0100

    Create function getRandom()

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+
+
 function getPrompt(){
     const input = prompt("select 'rock', 'paper', 'scissors'");
     return input.toLowerCase() || input; 
 }
 
-let playerSelection = getPrompt();
+function getRandom(){
+    
+
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    return  randomNumber === 1 ? 'rock' : 
+        randomNumber === 2 ? 'paper' :
+        'scissors';
+}
